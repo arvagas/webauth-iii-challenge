38fb58480f0a071a6fd3e53526ea580e1ace29f4
Arvin Agas <arvagas@gmail.com>
2019-09-18 17:33:28 -0400

    login endpoint functional

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -14,11 +14,26 @@ router.post('/register', (req, res) => {
 
   Users.add(newUser)
     .then(user => {
-      const token = mw.generateToken(newUser)
+      const token = mw.generateToken(user)
 
       res.status(201).json({ token })
     })
-    .catch(err => res.status(500).json(err))
+    .catch(err => res.status(500).json({ message: 'error registering' }))
+})
+
+router.post('/login', (req, res) => {
+  const { username, password }= req.body
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = mw.generateToken(user)
+
+        res.json({ token })
+      } else res.status(401).json({ message: 'You shall not pass!' })
+    })
+    .catch(err => res.status(500).json({ message: 'error logging in' }))
 })
 
 module.exports = router
